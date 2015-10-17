2b36c4f02dd2878b77374437165384986954becf
Oleksiy Ivanchuk <barjomet@barjomet.com>
2015-10-17 22:10:31 +0200

    review routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,25 @@
 Rails.application.routes.draw do
 
-  # Users
-  get 'users/current', to: 'users#current', defaults: {format: :json}
-  # resources :users, path: :users_test
-  resources :users, defaults: {format: :json}
-
   # Auth
   delete '/logout', to: 'sessions#destroy'
   get '/auth/:provider', to: 'sessions#create', as: 'login'
   get '/auth/:provider/callback', to: 'sessions#create', as: 'callback'
   get '/auth/failure', to: 'sessions#auth_failure'
 
-  # Items|Library
-  # resources :items, path: :items_test
-  resources :items, path: :items,  defaults: {format: :json}
+  # Items
+  resources :items, path: :items,  defaults: {format: :json}, except: [:edit, :new]
+
+  # Users
+  get 'users/current', to: 'users#current', defaults: {format: :json}
+  resources :users, defaults: {format: :json}, only: [:show]
+
+  # Debug-devepopment views
+  if Rails.env.development?
+    resources :items, path: :items_test
+    resources :users, path: :users_test
+  end
 
   root to: 'application#index'
   match '*all', to: 'application#index', via: [:get]
+
 end
