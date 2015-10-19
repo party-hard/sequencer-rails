d7b3a0d8b843bb5e3f55eb9416b87ac233357ea0
Oleksiy Ivanchuk <barjomet@barjomet.com>
2015-10-19 12:30:40 +0200

    Fix webpack initializer issue with bash on os x

--- config/initializers/webpack.rb
+++ config/initializers/webpack.rb
@@ -1,15 +1,6 @@
-if Rails.env.development?
+if Rails.env.development? and defined?(Rails::Server)
   Rails.application.config.before_initialize do
-    if defined?(Rails::Server)
       puts '=> Starting Webpack in watch mode'
-      `cd sequencer-react && npm run dev-rails > ../log/webpack.log & echo $! > tmp/pids/webpack.pid`
-    end
-  end
-
-  at_exit do
-    if defined?(Rails::Server)
-      puts "Exiting Webpack"
-      `/bin/kill -s SIGINT $(cat tmp/pids/webpack.pid) && rm tmp/pids/webpack.pid`
-    end
+      system('cd sequencer-react && npm run dev-rails > ../log/webpack.log &')
   end
 end
