b3a0e657eeaffaf472684b600a6d08deeaee57d3
Oleksiy Ivanchuk <barjomet@barjomet.com>
2015-10-27 19:42:18 +0200

    redirect to /#/users|items/:id if content type is html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,9 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    if request.headers['Accept'] =~ /html/
+      redirect_to root_url + "/#/users/#{params[:id]}"
+    end
     if @user[:id] != current_user[:id]
       @items = @user.items.where(shared: :true)
     else
