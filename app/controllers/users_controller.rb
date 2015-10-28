6765ad52553c0014aa609244cb889f89231c884e
Andrey Yavtushenko <andrew.yavtushenko@gmail.com>
2015-10-28 16:23:27 +0200

    allow get items for unauthenticated users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :login_required, except: :show :index
+  before_filter :login_required, :except => [:show, :index]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
